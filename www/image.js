15fd4c18801cf5e021c0b4d91986bbb2aa368f5d
wiktorpiela <76792178+wiktorpiela@users.noreply.github.com>
2022-08-11 09:10:47 +0200

    Delete www directory

--- www/image.js
+++
@@ -1,9 +0,0 @@
-function updateBgImage() {
-    if ($("li.active")[0].firstElementChild.dataset.value == "Home") {
-        document.body.style.backgroundImage = "tatry3.jpg"
-    } else {
-        document.body.style.background = "transparent"
-    }   
-}
-
-$("ul.nav").click(updateBgImage)
